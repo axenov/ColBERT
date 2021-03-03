bae0926e0ffb5224e9d04c62696059b173171457
Dmitry <dm.aksenowQgmail.com>
2021-03-03 17:43:37 +0100

    Upload run script

---
+++ run_colbert.sh
@@ -0,0 +1,38 @@
+train_data="eval.multi.triples.train.small.tsv"
+batch=16
+dim=64
+model=base
+experiment=base
+
+while getopts f:b:d:m:e: flag
+do
+    case "${flag}" in
+        f) train_data=${OPTARG};;
+        b) batch=${OPTARG};;
+        d) dim=${OPTARG};;
+		m) model=${OPTARG};;
+		e) experiment=${OPTARG};;
+    esac
+done
+
+if [[ $model = "large" ]]
+then
+  git checkout 0.2_large_cloud
+fi
+
+if [[ $model = "base" ]]
+then
+  git checkout 0.2_cloud
+fi
+
+mkdir -p DATA
+
+if test ! -f "DATA/${train_data}"; then
+    aws s3 cp "s3://datasets/german_ms_marco/${train_data}.zip" ./DATA --endpoint-url $AWS_ENDPOINT_URL
+    unzip "DATA/${train_data}.zip" -d "DATA/"
+    rm "DATA/${train_data}.zip"
+
+fi
+
+
+python -m colbert.train --triples DATA/${train_data} --query_maxlen 32 --doc_maxlen 150 --bsize $batch --dim $dim --amp --experiment $experiment
